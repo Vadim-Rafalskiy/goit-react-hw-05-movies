543ddd6208d478de0ef624fd7378f1a05b3012be
Vadim-Rafalskiy <vadim.rafalskiy.zaknafer@gmail.com>
2023-03-09 17:02:42 +0200

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, memo } from 'react';
 
 import PropTypes from 'prop-types';
 
@@ -42,7 +42,7 @@ const SearchBar = ({ onSubmit }) => {
   );
 };
 
-export default SearchBar;
+export default memo(SearchBar);
 
 SearchBar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
